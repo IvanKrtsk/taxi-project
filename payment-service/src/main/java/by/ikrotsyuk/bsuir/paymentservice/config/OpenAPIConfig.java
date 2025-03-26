e59224a81bdc992962ae0c26f9af4833d7c3f125
IvanKrtsk <ikrotsyuk@gmail.com>
2025-03-26 19:33:48 +0300

    add swagger config to new services

---
+++ payment-service/src/main/java/by/ikrotsyuk/bsuir/paymentservice/config/OpenAPIConfig.java
@@ -0,0 +1,26 @@
+package by.ikrotsyuk.bsuir.paymentservice.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+import java.util.List;
+
+@OpenAPIDefinition
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("http://localhost:8083"),
+                        new Server().url("http://localhost:8088")))
+                .info(new Info().title("Passenger Service API").version("1.0.0"));
+    }
+}
