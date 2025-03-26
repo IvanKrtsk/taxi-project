75d66a6fb71cd4a162edbb54a9904227588afd5c
IvanKrtsk <ikrotsyuk@gmail.com>
2025-03-26 15:23:06 +0300

    add rating-service and payment-service

---
+++ rating-service/src/test/java/by/ikrotsyuk/bsuir/ratingservice/RatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ikrotsyuk.bsuir.ratingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
