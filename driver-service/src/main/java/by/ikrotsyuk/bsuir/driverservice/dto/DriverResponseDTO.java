5311f47c5fdc11fb4ea37039508708ca93fac4ac
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-01 21:37:09 +0300

    add dtos vor vehicle and driver

---
+++ driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/dto/DriverResponseDTO.java
@@ -0,0 +1,37 @@
+package by.ikrotsyuk.bsuir.driverservice.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class DriverResponseDTO {
+    @Schema(description = "driver name")
+    @NotBlank
+    @Size(max = 100)
+    private String name;
+    @Schema(description = "driver email")
+    @NotBlank
+    @Column(unique = true)
+    @Size(max = 100)
+    private String email;
+    @Schema(description = "driver phone")
+    @NotBlank
+    @Size(max = 15)
+    private String phone;
+    @Schema(description = "driver rating")
+    @NotNull
+    @DecimalMin(value = "0.0")
+    @DecimalMax(value = "10.0")
+    private Double rating;
+    @Schema(description = "number of driver trips")
+    @NotNull
+    private Long total_rides;
+}
---
+++ driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/dto/VehicleRequestDTO.java
@@ -0,0 +1,48 @@
+package by.ikrotsyuk.bsuir.driverservice.dto;
+
+import by.ikrotsyuk.bsuir.driverservice.entity.customtypes.CarClassTypes;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class VehicleRequestDTO {
+    @Schema(description = "driver's car brand")
+    @NotBlank
+    @Size(max = 50)
+    private String brand;
+    @Schema(description = "driver's car model")
+    @NotBlank
+    @Size(max = 50)
+    private String model;
+    @Schema(description = "driver's car class")
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private CarClassTypes carClassTypes;
+    @Schema(description = "number of driver trips on this car")
+    @NotNull
+    private Long ridesCount;
+    @Schema(description = "date of manufacture of the car")
+    @NotNull
+    private Integer year;
+    @Schema(description = "car number plate")
+    @Column(unique = true)
+    @NotBlank
+    @Size(max = 20)
+    private String licensePlate;
+    @Schema(description = "driver's car color")
+    @NotBlank
+    @Size(max = 30)
+    private String color;
+}
