593eec348d8ebd2946c067143b86165620ed471c
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-01 21:42:04 +0300

    add annotations and dependencies in controllers

--- driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/controller/impl/DriverController.java
+++ driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/controller/impl/DriverController.java
@@ -1,9 +1,19 @@
 package by.ikrotsyuk.bsuir.driverservice.controller.impl;
 
 import by.ikrotsyuk.bsuir.driverservice.controller.DriverOperations;
+import by.ikrotsyuk.bsuir.driverservice.service.impl.DriverServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
+@Validated
+@RestController
+@RequestMapping("/api/v1/driver")
 public class DriverController implements DriverOperations {
+    private final DriverServiceImpl driverService;
     @Override
     public ResponseEntity<?> getDriverProfile(Long id) {
         return null;
--- driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/controller/impl/VehicleController.java
+++ driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/controller/impl/VehicleController.java
@@ -1,9 +1,19 @@
 package by.ikrotsyuk.bsuir.driverservice.controller.impl;
 
 import by.ikrotsyuk.bsuir.driverservice.controller.VehicleOperations;
+import by.ikrotsyuk.bsuir.driverservice.service.impl.VehicleServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
+@Validated
+@RestController
+@RequestMapping("/api/v1/vehicle")
 public class VehicleController implements VehicleOperations {
+    private final VehicleServiceImpl vehicleService;
     @Override
     public ResponseEntity<?> addVehicle() {
         return null;
