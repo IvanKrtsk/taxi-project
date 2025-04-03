7ab1ceab5daef05450bb7ac562fa19834730d7d8
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-03 22:33:17 +0300

    refactor entity and dto's

--- passenger-service/src/main/java/by/ikrotsyuk/bsuir/passengerservice/dto/PassengerRequestDTO.java
+++ passenger-service/src/main/java/by/ikrotsyuk/bsuir/passengerservice/dto/PassengerRequestDTO.java
@@ -2,29 +2,25 @@ package by.ikrotsyuk.bsuir.passengerservice.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-public class PassengerRequestDTO {
+public record PassengerRequestDTO(
     @Schema(description = "passenger name")
     @NotBlank
     @Size(max = 100)
-    private String name;
+    String name,
+
     @Schema(description = "passenger email")
     @Column(unique = true)
     @NotBlank
+    @Email
     @Size(max = 100)
-    private String email;
+    String email,
+
     @Schema(description = "passenger phone")
     @NotBlank
     @Size(max = 15)
-    private String phone;
-}
+    String phone
+){}
--- passenger-service/src/main/java/by/ikrotsyuk/bsuir/passengerservice/entity/PassengerEntity.java
+++ passenger-service/src/main/java/by/ikrotsyuk/bsuir/passengerservice/entity/PassengerEntity.java
@@ -21,36 +21,19 @@ import java.time.OffsetDateTime;
 @Setter
 @Builder
 public class PassengerEntity {
-    @Schema(description = "passenger id")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Schema(description = "passenger name")
-    @NotBlank
-    @Size(max = 100)
     private String name;
-    @Schema(description = "passenger email")
     @Column(unique = true)
-    @NotBlank
-    @Size(max = 100)
     private String email;
-    @Schema(description = "passenger phone")
     @Column(unique = true)
-    @NotBlank
-    @Size(max = 15)
     private String phone;
-    @Schema(description = "passenger rating")
-    @DecimalMin(value = "0.0")
-    @DecimalMax(value = "10.0")
     private Double rating;
-    @Schema(description = "passenger rides count")
     private Long totalRides;
-    @Schema(description = "is passenger account deleted")
     private Boolean isDeleted;
-    @Schema(description = "entity creation time")
     @CreationTimestamp
     private OffsetDateTime createdAt;
-    @Schema(description = "when was entity last updated date")
     @UpdateTimestamp
     private OffsetDateTime updatedAt;
 }
