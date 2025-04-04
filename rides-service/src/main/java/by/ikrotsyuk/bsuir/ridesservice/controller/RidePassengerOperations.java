b30fdff8f8beae587cba6ebbe8aa7e7001b07f11
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-04 12:10:34 +0300

    fix routing on gateway and do some test fixex in controller

--- rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/controller/RidePassengerOperations.java
+++ rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/controller/RidePassengerOperations.java
@@ -1,4 +1,14 @@
 package by.ikrotsyuk.bsuir.ridesservice.controller;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+
 public interface RidePassengerOperations {
+    ResponseEntity<BigDecimal> viewCostOfRide(@PathVariable Long id, @RequestParam @NotBlank @Size(max = 255) String startLocation, @RequestParam @NotBlank @Size(max = 255) String endLocation);
+
 }
--- rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/controller/impl/RidePassengerControllerImpl.java
+++ rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/controller/impl/RidePassengerControllerImpl.java
@@ -2,13 +2,24 @@ package by.ikrotsyuk.bsuir.ridesservice.controller.impl;
 
 import by.ikrotsyuk.bsuir.ridesservice.controller.RidePassengerOperations;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 @RequiredArgsConstructor
 @Validated
 @RestController
 @RequestMapping("/api/v1/rides/passenger")
 public class RidePassengerControllerImpl implements RidePassengerOperations {
+    @Override
+    @GetMapping("/{id}")
+    public ResponseEntity<BigDecimal> viewCostOfRide(@PathVariable Long id, String startLocation, String endLocation) {
+        return new ResponseEntity<>(BigDecimal.TEN, HttpStatus.OK);
+    }
 }
