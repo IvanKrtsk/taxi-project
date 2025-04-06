3c2335481a2b0193a5438778fb402f7ad20e01d4
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-06 22:07:43 +0300

    add service annotation on services

--- rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/service/impl/RatingReviewerServiceImpl.java
+++ rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/service/impl/RatingReviewerServiceImpl.java
@@ -5,7 +5,9 @@ import by.ikrotsyuk.bsuir.ratingservice.dto.RatingResponseDTO;
 import by.ikrotsyuk.bsuir.ratingservice.entity.customtypes.ReviewerTypesRating;
 import by.ikrotsyuk.bsuir.ratingservice.service.RatingReviewerService;
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RatingReviewerServiceImpl implements RatingReviewerService {
     @Override
     public RatingResponseDTO leaveReview(RatingRequestDTO ratingRequestDTO) {
