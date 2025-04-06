151832cd1ffe3264a258d10d14dfb8f2401275b6
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-06 23:40:29 +0300

    rating service done without exceptions

--- rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/controller/RatingAdminOperations.java
+++ rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/controller/RatingAdminOperations.java
@@ -1,5 +1,6 @@
 package by.ikrotsyuk.bsuir.ratingservice.controller;
 
+import by.ikrotsyuk.bsuir.ratingservice.dto.RatingAdminResponseDTO;
 import by.ikrotsyuk.bsuir.ratingservice.dto.RatingRequestDTO;
 import by.ikrotsyuk.bsuir.ratingservice.dto.RatingResponseDTO;
 import jakarta.validation.Valid;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface RatingAdminOperations {
-    ResponseEntity<RatingResponseDTO> getRatingById(@RequestParam String id);
-    ResponseEntity<Page<RatingResponseDTO>> getAllRatings(@RequestParam int offset, @RequestParam int itemCount, @RequestParam(required = false) String field, @RequestParam(required = false) Boolean isSortDirectionAsc);
-    ResponseEntity<RatingResponseDTO> editRating(@RequestParam String id, @Valid @RequestBody RatingRequestDTO requestDTO);
-    ResponseEntity<RatingResponseDTO> deleteRating(@RequestParam String id);
+    ResponseEntity<RatingAdminResponseDTO> getRatingById(@RequestParam String id);
+    ResponseEntity<Page<RatingAdminResponseDTO>> getAllRatings(@RequestParam int offset, @RequestParam int itemCount, @RequestParam(required = false) String field, @RequestParam(required = false) Boolean isSortDirectionAsc);
+    ResponseEntity<RatingAdminResponseDTO> editRating(@RequestParam String id, @Valid @RequestBody RatingRequestDTO requestDTO);
+    ResponseEntity<RatingAdminResponseDTO> deleteRating(@RequestParam String id);
 }
--- rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/controller/impl/RatingAdminController.java
+++ rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/controller/impl/RatingAdminController.java
@@ -1,6 +1,7 @@
 package by.ikrotsyuk.bsuir.ratingservice.controller.impl;
 
 import by.ikrotsyuk.bsuir.ratingservice.controller.RatingAdminOperations;
+import by.ikrotsyuk.bsuir.ratingservice.dto.RatingAdminResponseDTO;
 import by.ikrotsyuk.bsuir.ratingservice.dto.RatingRequestDTO;
 import by.ikrotsyuk.bsuir.ratingservice.dto.RatingResponseDTO;
 import by.ikrotsyuk.bsuir.ratingservice.service.RatingAdminService;
@@ -21,25 +22,25 @@ public class RatingAdminController implements RatingAdminOperations {
 
     @Override
     @GetMapping
-    public ResponseEntity<RatingResponseDTO> getRatingById(String id) {
-        return new ResponseEntity<>(ratingAdminService.getRatingById(id), HttpStatus.OK);
+    public ResponseEntity<RatingAdminResponseDTO> getRatingById(String id) {
+        return new ResponseEntity<>(ratingAdminService.getReviewById(id), HttpStatus.OK);
     }
 
     @Override
     @GetMapping("/all")
-    public ResponseEntity<Page<RatingResponseDTO>> getAllRatings(int offset, int itemCount, String field, Boolean isSortDirectionAsc) {
-        return new ResponseEntity<>(ratingAdminService.getAllRatings(offset, itemCount, field, isSortDirectionAsc), HttpStatus.OK);
+    public ResponseEntity<Page<RatingAdminResponseDTO>> getAllRatings(int offset, int itemCount, String field, Boolean isSortDirectionAsc) {
+        return new ResponseEntity<>(ratingAdminService.getAllReviews(offset, itemCount, field, isSortDirectionAsc), HttpStatus.OK);
     }
 
     @Override
     @PatchMapping
-    public ResponseEntity<RatingResponseDTO> editRating(String id, @Valid @RequestBody RatingRequestDTO requestDTO) {
-        return new ResponseEntity<>(ratingAdminService.editRating(id, requestDTO), HttpStatus.OK);
+    public ResponseEntity<RatingAdminResponseDTO> editRating(String id, @Valid @RequestBody RatingRequestDTO requestDTO) {
+        return new ResponseEntity<>(ratingAdminService.editReview(id, requestDTO), HttpStatus.OK);
     }
 
     @Override
     @DeleteMapping
-    public ResponseEntity<RatingResponseDTO> deleteRating(String id) {
-        return new ResponseEntity<>(ratingAdminService.deleteRating(id), HttpStatus.OK);
+    public ResponseEntity<RatingAdminResponseDTO> deleteRating(String id) {
+        return new ResponseEntity<>(ratingAdminService.deleteReview(id), HttpStatus.OK);
     }
 }
