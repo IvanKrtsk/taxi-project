319518a1a2d5ab63e5f73884d43e57f23e024c74
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-06 22:02:34 +0300

    update controller methods(replace params)

--- rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/controller/RatingReviewerOperations.java
+++ rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/controller/RatingReviewerOperations.java
@@ -1,12 +1,16 @@
 package by.ikrotsyuk.bsuir.ratingservice.controller;
 
+import by.ikrotsyuk.bsuir.ratingservice.dto.RatingRequestDTO;
 import by.ikrotsyuk.bsuir.ratingservice.dto.RatingResponseDTO;
+import by.ikrotsyuk.bsuir.ratingservice.entity.customtypes.ReviewerTypesRating;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface RatingReviewerOperations {
-    ResponseEntity<RatingResponseDTO> leaveReview(@PathVariable Long reviewerId, @RequestParam Long rideId);
-    ResponseEntity<Page<RatingResponseDTO>> viewLeavedReviews(@PathVariable Long reviewerId);
+    ResponseEntity<RatingResponseDTO> leaveReview(@Valid @RequestBody RatingRequestDTO ratingRequestDTO);
+    ResponseEntity<Page<RatingResponseDTO>> viewLeavedReviews(@PathVariable Long reviewerId, @RequestParam ReviewerTypesRating reviewerType);
 }
--- rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/controller/impl/RatingAdminController.java
+++ rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/controller/impl/RatingAdminController.java
@@ -3,9 +3,17 @@ package by.ikrotsyuk.bsuir.ratingservice.controller.impl;
 import by.ikrotsyuk.bsuir.ratingservice.controller.RatingAdminOperations;
 import by.ikrotsyuk.bsuir.ratingservice.dto.RatingRequestDTO;
 import by.ikrotsyuk.bsuir.ratingservice.dto.RatingResponseDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
+@Validated
+@RestController
+@RequestMapping("/api/v1/rating/admin")
 public class RatingAdminController implements RatingAdminOperations {
 
     @Override
