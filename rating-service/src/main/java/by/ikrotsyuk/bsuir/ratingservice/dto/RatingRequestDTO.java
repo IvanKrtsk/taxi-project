5bd06e9403024d56bde1cc74f1e5c8d95d0d9fa3
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-07 21:31:40 +0300

    add @Min(1) constraint in request dto

--- rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/dto/RatingRequestDTO.java
+++ rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/dto/RatingRequestDTO.java
@@ -2,17 +2,16 @@ package by.ikrotsyuk.bsuir.ratingservice.dto;
 
 import by.ikrotsyuk.bsuir.ratingservice.entity.customtypes.ReviewerTypesRating;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.DecimalMax;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record RatingRequestDTO(
         @Schema(description = "id of the rated trip")
+        @Min(1)
         @NotNull
         Long rideId,
 
         @Schema(description = "reviewer id")
+        @Min(1)
         @NotNull
         Long reviewerId,
 
