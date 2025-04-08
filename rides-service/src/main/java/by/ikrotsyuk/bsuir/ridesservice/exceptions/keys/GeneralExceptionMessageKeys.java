b764a57c56bf7e8befe52f0277eddad6d8023353
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-08 16:20:51 +0300

    add field deserialization error message

--- rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/exceptions/keys/GeneralExceptionMessageKeys.java
+++ rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/exceptions/keys/GeneralExceptionMessageKeys.java
@@ -7,7 +7,8 @@ public enum GeneralExceptionMessageKeys {
     RIDE_NOT_FOUND_BY_ID_MESSAGE_KEY("ride.not.found.by.id.message"),
     RIDES_NOT_FOUND_MESSAGE_KEY("rides.not.found.message"),
     METHOD_ARGUMENT_TYPE_MISMATCH_MESSAGE_KEY("method.argument.type.mismatch.message"),
-    ENUM_ARGUMENT_DESERIALIZATION_MESSAGE_KEY("enum.argument.deserialization.message");
+    ENUM_ARGUMENT_DESERIALIZATION_MESSAGE_KEY("enum.argument.deserialization.message"),
+    FIELD_DESERIALIZATION_MESSAGE_KEY("field.deserialization.message");
 
     private final String messageKey;
 
