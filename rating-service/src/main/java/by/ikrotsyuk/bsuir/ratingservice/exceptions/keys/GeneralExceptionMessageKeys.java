e3aa87ff44c0e029b14c6013590300907197340e
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-08 10:47:45 +0300

    add exception message(deserializing field)

--- rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/exceptions/keys/GeneralExceptionMessageKeys.java
+++ rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/exceptions/keys/GeneralExceptionMessageKeys.java
@@ -8,7 +8,8 @@ public enum GeneralExceptionMessageKeys {
     REVIEWS_NOT_FOUND_MESSAGE_KEY("reviews.not.found.message"),
     ID_IS_NOT_VALID_MESSAGE_KEY("id.is.not.valid.message"),
     METHOD_ARGUMENT_TYPE_MISMATCH_MESSAGE_KEY("method.argument.type.mismatch.message"),
-    ENUM_ARGUMENT_DESERIALIZATION_MESSAGE_KEY("enum.argument.deserialization.message");
+    ENUM_ARGUMENT_DESERIALIZATION_MESSAGE_KEY("enum.argument.deserialization.message"),
+    FIELD_DESERIALIZATION_MESSAGE_KEY("field.deserialization.message");
 
     private final String messageKey;
 
