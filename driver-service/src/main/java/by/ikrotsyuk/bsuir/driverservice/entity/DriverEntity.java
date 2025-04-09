91f9a644e26c2110fcd16f02f3abf88c514e888b
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-09 18:45:43 +0300

    add lombok FieldNameConstants to DriverEntity and VehicleEntity

--- driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/entity/DriverEntity.java
+++ driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/entity/DriverEntity.java
@@ -3,6 +3,7 @@ package by.ikrotsyuk.bsuir.driverservice.entity;
 import by.ikrotsyuk.bsuir.driverservice.entity.customtypes.StatusTypesDriver;
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.FieldNameConstants;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.JdbcType;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "drivers")
+@FieldNameConstants
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
--- driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/entity/VehicleEntity.java
+++ driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/entity/VehicleEntity.java
@@ -4,6 +4,7 @@ import by.ikrotsyuk.bsuir.driverservice.entity.customtypes.CarClassTypesDriver;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.FieldNameConstants;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.JdbcType;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -13,6 +14,7 @@ import java.time.OffsetDateTime;
 
 @Entity
 @Table(name = "vehicles")
+@FieldNameConstants
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
