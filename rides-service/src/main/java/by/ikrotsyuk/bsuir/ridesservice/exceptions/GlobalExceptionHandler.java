7e7f4590442bde42a4a5cc64032f4ecb3ac2ef1a
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-09 21:07:49 +0300

    update exception handler

--- rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/exceptions/GlobalExceptionHandler.java
+++ rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/exceptions/GlobalExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -29,36 +30,20 @@ public class GlobalExceptionHandler {
     private final MessageSource messageSource;
     private final String DTO_PACKAGE_PATH = "by.ikrotsyuk.bsuir.ridesservice.dto.";
 
-    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ExceptionDTO> handleMissingServletRequestParameterException(MissingServletRequestParameterException ex) {
+        String messageKey = GeneralExceptionMessageKeys.ENUM_ARGUMENT_DESERIALIZATION_MESSAGE_KEY.getMessageKey();
+        String message = messageSource.getMessage(messageKey, new Object[]{ex.getParameterName(), ex.getMethodParameter(), ex.getParameterType()}, LocaleContextHolder.getLocale());
+        return new ResponseEntity<>(new ExceptionDTO(message, messageKey), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
-            if (error instanceof FieldError) {
-                FieldError fieldError = (FieldError) error;
-                String fieldName = fieldError.getField();
-                Object rejectedValue = fieldError.getRejectedValue();
-                String errorMessage;
-                try {
-                    String fullQualifiedName = DTO_PACKAGE_PATH + capitalize(fieldError.getObjectName());
-
-                    Class<?> dtoClass = Class.forName(fullQualifiedName);
-                    Field field = dtoClass.getDeclaredField(fieldName);
-                    Class<?> fieldType = field.getType();
-
-                    if (fieldType.isEnum()) {
-                        Object[] enumValues = fieldType.getEnumConstants();
-                        String possibleValues = Arrays.toString(enumValues);
-                        errorMessage = messageSource.getMessage(GeneralExceptionMessageKeys.ENUM_ARGUMENT_DESERIALIZATION_MESSAGE_KEY.getMessageKey(),
-                                new Object[]{fieldName, rejectedValue, possibleValues},
-                                LocaleContextHolder.getLocale());
-                    } else {
-                        errorMessage = fieldError.getDefaultMessage();
-                    }
-                } catch (ClassNotFoundException | NoSuchFieldException e) {
-                    errorMessage = messageSource.getMessage(GeneralExceptionMessageKeys.FIELD_DESERIALIZATION_MESSAGE_KEY.getMessageKey(),
-                            new Object[]{fieldError.getRejectedValue()}, LocaleContextHolder.getLocale());
-                }
-                errors.put(fieldName, errorMessage);
+            if (error instanceof FieldError fieldError) {
+                String errorMessage = generateFieldErrorMessage(fieldError);
+                errors.put(fieldError.getField(), errorMessage);
             }
         });
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
@@ -67,37 +52,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<Map<String, String>> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
         Map<String, String> errors = new HashMap<>();
-
-        String rejectedValue;
         Throwable cause = ex.getCause();
+
         if (cause instanceof InvalidFormatException invalidFormatException) {
-            Class<?> targetType = invalidFormatException.getTargetType();
-            rejectedValue = invalidFormatException.getValue().toString();
+            String errorMessage = handleInvalidFormatException(invalidFormatException);
             String fieldName = invalidFormatException.getPath().get(0).getFieldName();
-            if (targetType.isEnum()) {
-                Object[] enumValues = targetType.getEnumConstants();
-                String possibleValues = Arrays.toString(enumValues);
-
-                String errorMessage = messageSource.getMessage(GeneralExceptionMessageKeys.ENUM_ARGUMENT_DESERIALIZATION_MESSAGE_KEY.getMessageKey(),
-                        new Object[]{fieldName, rejectedValue, possibleValues},
-                        LocaleContextHolder.getLocale());
-
-                errors.put(fieldName, errorMessage);
-            } else {
-                errors.put(fieldName, messageSource.getMessage(GeneralExceptionMessageKeys.FIELD_DESERIALIZATION_MESSAGE_KEY.getMessageKey(),
-                        new Object[]{rejectedValue}, LocaleContextHolder.getLocale()));
-            }
+            errors.put(fieldName, errorMessage);
         } else {
             errors.put("error", ex.getMessage());
         }
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
-
-
-    private String capitalize(String name) {
-        return name.substring(0, 1).toUpperCase() + name.substring(1);
-    }
-
 
     @ExceptionHandler({AvailableRidesNotFoundException.class, CurrentRideNotFoundException.class, RideNotFoundByIdException.class, RidesNotFoundException.class})
     public ResponseEntity<ExceptionDTO> handleRideNotFoundException(ExceptionTemplate ex){
@@ -121,5 +86,58 @@ public class GlobalExceptionHandler {
         String message = messageSource
                 .getMessage(messageKey, new Object[]{ex.getName(), ex.getRequiredType(), ex.getValue()}, LocaleContextHolder.getLocale());
         return new ResponseEntity<>(new ExceptionDTO(message, messageKey), HttpStatus.BAD_REQUEST);
+    }
+
+    private String generateFieldErrorMessage(FieldError fieldError) {
+        String fieldName = fieldError.getField();
+        Object rejectedValue = fieldError.getRejectedValue();
+        try {
+            String fullQualifiedName = DTO_PACKAGE_PATH + capitalize(fieldError.getObjectName());
+            Class<?> dtoClass = Class.forName(fullQualifiedName);
+            Field field = dtoClass.getDeclaredField(fieldName);
+            Class<?> fieldType = field.getType();
+
+            if (fieldType.isEnum()) {
+                return generateEnumErrorMessage(fieldType, fieldName, rejectedValue);
+            } else {
+                return fieldError.getDefaultMessage();
+            }
+        } catch (ClassNotFoundException | NoSuchFieldException e) {
+            return messageSource.getMessage(
+                    GeneralExceptionMessageKeys.FIELD_DESERIALIZATION_MESSAGE_KEY.getMessageKey(),
+                    new Object[]{rejectedValue},
+                    LocaleContextHolder.getLocale()
+            );
+        }
+    }
+
+    private String handleInvalidFormatException(InvalidFormatException ex) {
+        String fieldName = ex.getPath().get(0).getFieldName();
+        Object rejectedValue = ex.getValue();
+        Class<?> targetType = ex.getTargetType();
+
+        if (targetType.isEnum()) {
+            return generateEnumErrorMessage(targetType, fieldName, rejectedValue);
+        } else {
+            return messageSource.getMessage(
+                    GeneralExceptionMessageKeys.FIELD_DESERIALIZATION_MESSAGE_KEY.getMessageKey(),
+                    new Object[]{rejectedValue},
+                    LocaleContextHolder.getLocale()
+            );
+        }
+    }
+
+    private String capitalize(String name) {
+        return name.substring(0, 1).toUpperCase() + name.substring(1);
+    }
+
+    private String generateEnumErrorMessage(Class<?> fieldType, String fieldName, Object rejectedValue) {
+        Object[] enumValues = fieldType.getEnumConstants();
+        String possibleValues = Arrays.toString(enumValues);
+        return messageSource.getMessage(
+                GeneralExceptionMessageKeys.ENUM_ARGUMENT_DESERIALIZATION_MESSAGE_KEY.getMessageKey(),
+                new Object[]{fieldName, rejectedValue, possibleValues},
+                LocaleContextHolder.getLocale()
+        );
     }
 }
