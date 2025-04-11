fd7a7a696873ca8a3ef5df5b073c9a8e49d4cb1c
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-11 19:36:59 +0300

    remove wildcards

--- rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/controller/RidePassengerOperations.java
+++ rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/controller/RidePassengerOperations.java
@@ -6,7 +6,12 @@ import by.ikrotsyuk.bsuir.ridesservice.dto.RideResponseDTO;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.math.BigDecimal;
 
--- rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/service/RideAdminService.java
+++ rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/service/RideAdminService.java
@@ -3,7 +3,6 @@ package by.ikrotsyuk.bsuir.ridesservice.service;
 import by.ikrotsyuk.bsuir.ridesservice.dto.RideFullRequestDTO;
 import by.ikrotsyuk.bsuir.ridesservice.dto.RideFullResponseDTO;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.RequestParam;
 
 public interface RideAdminService {
     RideFullResponseDTO editRide(Long rideId, RideFullRequestDTO rideFullRequestDTO);
