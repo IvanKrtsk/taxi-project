ef72b466507c0263d8e3799cc2f35898c7b1e643
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-11 19:17:19 +0300

    remove wildcards

--- rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/controller/impl/RideAdminController.java
+++ rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/controller/impl/RideAdminController.java
@@ -8,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
--- rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/dto/RideFullRequestDTO.java
+++ rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/dto/RideFullRequestDTO.java
@@ -4,7 +4,10 @@ import by.ikrotsyuk.bsuir.ridesservice.entity.customtypes.CarClassTypesRides;
 import by.ikrotsyuk.bsuir.ridesservice.entity.customtypes.PaymentTypeTypesRides;
 import by.ikrotsyuk.bsuir.ridesservice.entity.customtypes.RideStatusTypesRides;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 
