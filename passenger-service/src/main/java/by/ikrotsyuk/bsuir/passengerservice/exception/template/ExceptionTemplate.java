56106bdda33f279312a24fcf0bc62fd4bfc6d60b
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-11 19:34:13 +0300

    add manual cunstroctor instead of @requiredargsconstructor

--- passenger-service/src/main/java/by/ikrotsyuk/bsuir/passengerservice/exception/template/ExceptionTemplate.java
+++ passenger-service/src/main/java/by/ikrotsyuk/bsuir/passengerservice/exception/template/ExceptionTemplate.java
@@ -1,11 +1,14 @@
 package by.ikrotsyuk.bsuir.passengerservice.exception.template;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class ExceptionTemplate extends RuntimeException {
     private final String messageKey;
     private final Object[] args;
+
+    public ExceptionTemplate(String messageKey, Object... args){
+        this.messageKey = messageKey;
+        this.args = args;
+    }
 }
