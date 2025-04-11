c1bdb3a1dba1a3165ac6d13a62000681689d64ac
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-11 23:06:01 +0300

    change patch mapping -> put mapping in ride operations

--- rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/controller/RideOperations.java
+++ rides-service/src/main/java/by/ikrotsyuk/bsuir/ridesservice/controller/RideOperations.java
@@ -5,15 +5,10 @@ import by.ikrotsyuk.bsuir.ridesservice.dto.RideFullResponseDTO;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 public interface RideOperations {
-    @PatchMapping("/{rideId}")
+    @PutMapping("/{rideId}")
     ResponseEntity<RideFullResponseDTO> editRide(@PathVariable Long rideId, @Valid @RequestBody RideFullRequestDTO rideFullRequestDTO);
 
     @DeleteMapping("/{rideId}")
