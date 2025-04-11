57f9f3f9c3662b8be69ef8ea43a299fa3b7c9a3e
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-11 19:25:39 +0300

    add @requiredArgsConstructor instead of manual constructor

--- driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/exception/template/ExceptionTemplate.java
+++ driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/exception/template/ExceptionTemplate.java
@@ -1,15 +1,11 @@
 package by.ikrotsyuk.bsuir.driverservice.exception.template;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class ExceptionTemplate extends RuntimeException {
     private final String messageKey;
     private final Object[] args;
-
-    public ExceptionTemplate(String messageKey, Object... args) {
-        super(messageKey);
-        this.messageKey = messageKey;
-        this.args = args;
-    }
 }
