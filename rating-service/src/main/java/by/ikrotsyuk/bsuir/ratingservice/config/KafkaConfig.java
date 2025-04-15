89af220d83cc172da781d0aca83f7881fc7e7948
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-15 16:02:16 +0300

    add topics config class

---
+++ rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/config/KafkaConfig.java
@@ -0,0 +1,29 @@
+package by.ikrotsyuk.bsuir.ratingservice.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public NewTopic createDriverRatingUpdatedTopic(){
+        return TopicBuilder.name("driver-rating-updated-event-topic")
+                .partitions(3)
+                .replicas(3)
+                .configs(Map.of("min.insync.replicas", "2"))
+                .build();
+    }
+
+    @Bean
+    public NewTopic createPassengerRatingUpdatedTopic(){
+        return TopicBuilder.name("passenger-rating-updated-event-topic")
+                .partitions(3)
+                .replicas(3)
+                .configs(Map.of("min.insync.replicas", "2"))
+                .build();
+    }
+}
