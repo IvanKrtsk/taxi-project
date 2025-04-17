d585d6c14732de311f68b000c308e60fed4df3e4
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-17 21:48:47 +0300

    update kafka config and add scheduler config

--- rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/config/KafkaConfig.java
+++ rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/config/KafkaConfig.java
@@ -1,5 +1,6 @@
 package by.ikrotsyuk.bsuir.ratingservice.config;
 
+import by.ikrotsyuk.bsuir.ratingservice.kafka.KafkaConstants;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,19 +12,19 @@ import java.util.Map;
 public class KafkaConfig {
     @Bean
     public NewTopic createDriverRatingUpdatedTopic(){
-        return TopicBuilder.name("driver-rating-updated-event-topic")
-                .partitions(3)
-                .replicas(3)
-                .configs(Map.of("min.insync.replicas", "2"))
+        return TopicBuilder.name(KafkaConstants.DRIVER_RATING_TOPIC_NAME)
+                .partitions(KafkaConstants.NUMBER_OF_PARTITIONS)
+                .replicas(KafkaConstants.NUMBER_OF_REPLICAS)
+                .configs(Map.of(KafkaConstants.NUMBER_OF_INSYNC_REPLICAS_KEY, KafkaConstants.NUMBER_OF_INSYNC_REPLICAS_VALUE))
                 .build();
     }
 
     @Bean
     public NewTopic createPassengerRatingUpdatedTopic(){
-        return TopicBuilder.name("passenger-rating-updated-event-topic")
-                .partitions(3)
-                .replicas(3)
-                .configs(Map.of("min.insync.replicas", "2"))
+        return TopicBuilder.name(KafkaConstants.PASSENGER_RATING_TOPIC_NAME)
+                .partitions(KafkaConstants.NUMBER_OF_PARTITIONS)
+                .replicas(KafkaConstants.NUMBER_OF_REPLICAS)
+                .configs(Map.of(KafkaConstants.NUMBER_OF_INSYNC_REPLICAS_KEY, KafkaConstants.NUMBER_OF_INSYNC_REPLICAS_VALUE))
                 .build();
     }
 }
