bd4f5bc36b149237802618e7de2d44ad3cf0fc07
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-17 21:50:00 +0300

    add kafka rating producer

---
+++ rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/kafka/producer/RatingProducerImpl.java
@@ -0,0 +1,87 @@
+package by.ikrotsyuk.bsuir.ratingservice.kafka.producer;
+
+import by.ikrotsyuk.bsuir.ratingservice.entity.UnsentRatingEntity;
+import by.ikrotsyuk.bsuir.ratingservice.entity.customtypes.ReviewerTypeTypes;
+import by.ikrotsyuk.bsuir.ratingservice.event.RatingUpdatedEvent;
+import by.ikrotsyuk.bsuir.ratingservice.kafka.KafkaAvailabilityChecker;
+import by.ikrotsyuk.bsuir.ratingservice.kafka.KafkaConstants;
+import by.ikrotsyuk.bsuir.ratingservice.kafka.producer.impl.RatingProducer;
+import by.ikrotsyuk.bsuir.ratingservice.mapper.UnsentRatingMapper;
+import by.ikrotsyuk.bsuir.ratingservice.repository.UnsentRatingRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+
+@RequiredArgsConstructor
+@Component
+public class RatingProducerImpl implements RatingProducer {
+    private final KafkaTemplate<String, RatingUpdatedEvent> kafkaTemplate;
+    private final UnsentRatingRepository unsentRatingRepository;
+    private final UnsentRatingMapper unsentRatingMapper;
+    private final KafkaAvailabilityChecker availabilityChecker;
+    private final List<UnsentRatingEntity> unsentRatingEntityList = new LinkedList<>();
+    private boolean isStartingUp = true;
+
+    @PostConstruct
+    void init(){
+        availabilityChecker.setRatingProducer(this);
+    }
+
+    public void sendRatingUpdatedEvent(ObjectId reviewId, RatingUpdatedEvent event){
+        if(availabilityChecker.isBrokersAvailable()) {
+            if(isStartingUp) {
+                sendUnsentMessages();
+                isStartingUp = !isStartingUp;
+            }
+            String topicName = (event.reviewerType() == ReviewerTypeTypes.DRIVER) ? KafkaConstants.DRIVER_RATING_TOPIC_NAME : KafkaConstants.PASSENGER_RATING_TOPIC_NAME;
+            CompletableFuture<SendResult<String, RatingUpdatedEvent>> future = kafkaTemplate.send(topicName, reviewId.toString(), event);
+            future.whenComplete((result, exception) -> {
+                if (exception != null) {
+                    saveMessageForScheduledSending(reviewId, event, exception.getMessage());
+                    availabilityChecker.setBrokersAvailable(false);
+                }
+            });
+        }else
+            saveMessageForScheduledSending(reviewId, event, null);
+    }
+
+    public void saveMessageForScheduledSending(ObjectId reviewId, RatingUpdatedEvent event, String exceptionMessage){
+        UnsentRatingEntity unsentRatingEntity = unsentRatingMapper.toEntity(event, reviewId);
+        if(!Objects.isNull(exceptionMessage))
+            unsentRatingEntity.setExceptionMessage(exceptionMessage);
+        if (unsentRatingEntityList.stream()
+                .noneMatch(item -> item.getId() == unsentRatingEntity.getId()))
+            unsentRatingEntityList.add(unsentRatingEntity);
+        if (!unsentRatingRepository.existsById(reviewId))
+            unsentRatingRepository.save(unsentRatingEntity);
+    }
+
+    public void sendUnsentMessages(){
+        System.out.println();
+        System.err.println("sendUnsentMessages start");
+        System.out.println();
+        if(!unsentRatingEntityList.isEmpty()){
+            unsentRatingEntityList.forEach(item ->
+                sendRatingUpdatedEvent(item.getId(), unsentRatingMapper.toEvent(item))
+            );
+            unsentRatingRepository.deleteAll(unsentRatingEntityList);
+            unsentRatingEntityList.clear();
+        }
+        List<UnsentRatingEntity> unsentRatingEntities = unsentRatingRepository.findAll();
+        if (unsentRatingEntities.isEmpty())
+            return;
+        unsentRatingEntities.forEach(item -> {
+            RatingUpdatedEvent event = unsentRatingMapper.toEvent(item);
+            unsentRatingRepository.deleteById(item.getId());
+            sendRatingUpdatedEvent(item.getId(), event);
+        });
+    }
+}
---
+++ rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/kafka/producer/impl/RatingProducer.java
@@ -0,0 +1,10 @@
+package by.ikrotsyuk.bsuir.ratingservice.kafka.producer.impl;
+
+import by.ikrotsyuk.bsuir.ratingservice.event.RatingUpdatedEvent;
+import org.bson.types.ObjectId;
+
+public interface RatingProducer {
+    void sendRatingUpdatedEvent(ObjectId reviewId, RatingUpdatedEvent event);
+    void saveMessageForScheduledSending(ObjectId reviewId, RatingUpdatedEvent event, String exceptionMessage);
+    void sendUnsentMessages();
+}
