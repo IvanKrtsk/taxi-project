9ef86fa8ab35e8cb1d925261e2054b3e728eeab2
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-17 10:29:18 +0300

    update rating updated event

--- rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/event/RatingUpdatedEvent.java
+++ rating-service/src/main/java/by/ikrotsyuk/bsuir/ratingservice/event/RatingUpdatedEvent.java
@@ -3,6 +3,8 @@ package by.ikrotsyuk.bsuir.ratingservice.event;
 import by.ikrotsyuk.bsuir.ratingservice.entity.customtypes.ReviewerTypeTypes;
 
 public record RatingUpdatedEvent(
+        Long rideId,
+
         Long reviewerId,
 
         Double rating,
