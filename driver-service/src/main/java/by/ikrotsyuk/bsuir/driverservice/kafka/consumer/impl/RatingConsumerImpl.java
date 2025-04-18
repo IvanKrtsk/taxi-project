bbfa728e2f1708f86afea48e0cea0df103d432ee
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-18 15:48:32 +0300

    add default consumer

---
+++ driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/kafka/consumer/impl/RatingConsumerImpl.java
@@ -0,0 +1,16 @@
+package by.ikrotsyuk.bsuir.driverservice.kafka.consumer.impl;
+
+import by.ikrotsyuk.bsuir.driverservice.event.RatingUpdatedEvent;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RatingConsumerImpl {
+
+    @KafkaListener(topics = "driver-rating-updated-event-topic")
+    public void handleRatingChange(RatingUpdatedEvent event){
+        System.out.println();
+        System.err.println(event);
+        System.out.println();
+    }
+}
