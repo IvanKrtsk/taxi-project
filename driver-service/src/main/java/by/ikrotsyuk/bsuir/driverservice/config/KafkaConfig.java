222ac4253d5d5bdb52ffb5984ab6811a6b3a7e29
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-20 18:29:24 +0300

    replace local event to global

--- driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/config/KafkaConfig.java
+++ driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/config/KafkaConfig.java
@@ -26,7 +26,6 @@ public class KafkaConfig {
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
         config.put(JsonDeserializer.TRUSTED_PACKAGES, KafkaConstants.TRUSTED_PACKAGES);
-        config.put(JsonDeserializer.VALUE_DEFAULT_TYPE, KafkaConstants.JSON_VALUE_DEFAULT_TYPE);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, KafkaConstants.CONSUMER_GROUP_ID);
 
         return new DefaultKafkaConsumerFactory<>(config);
--- driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/kafka/consumer/impl/RatingConsumerImpl.java
+++ driver-service/src/main/java/by/ikrotsyuk/bsuir/driverservice/kafka/consumer/impl/RatingConsumerImpl.java
@@ -1,6 +1,6 @@
 package by.ikrotsyuk.bsuir.driverservice.kafka.consumer.impl;
 
-import by.ikrotsyuk.bsuir.driverservice.event.RatingUpdatedEvent;
+import by.ikrotsyuk.bsuir.communicationparts.event.RatingUpdatedEvent;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
