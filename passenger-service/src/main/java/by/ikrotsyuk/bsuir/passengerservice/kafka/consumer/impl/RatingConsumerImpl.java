f7e343567d01a066eb7c9a576ab54829de6f9821
IvanKrtsk <ikrotsyuk@gmail.com>
2025-04-20 22:44:52 +0300

    add kafka listener annotation

--- passenger-service/src/main/java/by/ikrotsyuk/bsuir/passengerservice/kafka/consumer/impl/RatingConsumerImpl.java
+++ passenger-service/src/main/java/by/ikrotsyuk/bsuir/passengerservice/kafka/consumer/impl/RatingConsumerImpl.java
@@ -6,13 +6,16 @@ import by.ikrotsyuk.bsuir.passengerservice.kafka.KafkaConstants;
 import by.ikrotsyuk.bsuir.passengerservice.kafka.consumer.RatingConsumer;
 import by.ikrotsyuk.bsuir.passengerservice.service.KafkaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
 public class RatingConsumerImpl implements RatingConsumer {
     private final KafkaService kafkaService;
+
     @Override
+    @KafkaListener(topics = KafkaConstants.TOPIC_NAME)
     public void handleRatingChange(RatingUpdatedEvent event) {
         if(!kafkaService.updateRating(event))
             throw new NonRetryableException(KafkaConstants.NON_RETRYABLE_EXCEPTION_TEXT);
