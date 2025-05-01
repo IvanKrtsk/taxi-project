0dbf739cb0c310224db2369a89887ac7edc54a08
IvanKrtsk <ikrotsyuk@gmail.com>
2025-05-01 14:38:42 +0300

    move promo codes to relation db

--- payment-service/src/main/java/by/ikrotsyuk/bsuir/paymentservice/entity/PromoCodeEntity.java
+++ payment-service/src/main/java/by/ikrotsyuk/bsuir/paymentservice/entity/PromoCodeEntity.java
@@ -1,18 +1,14 @@
 package by.ikrotsyuk.bsuir.paymentservice.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.Instant;
 import java.time.OffsetDateTime;
 
 @AllArgsConstructor
@@ -20,19 +16,21 @@ import java.time.OffsetDateTime;
 @Getter
 @Setter
 @Builder
-@Document(collection = "promo_codes")
+@Entity
+@Table(name = "promo_codes")
 public class PromoCodeEntity {
     @Id
-    private ObjectId id;
-    @Indexed(unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
     private String code;
     private Double discountPercentage;
     private Long activationsCount;
-    private Instant startDate;
-    private Instant endDate;
+    private OffsetDateTime startDate;
+    private OffsetDateTime endDate;
     private Boolean isActive;
-    @CreatedDate
+    @CreationTimestamp
     private OffsetDateTime createdAt;
-    @LastModifiedDate
+    @UpdateTimestamp
     private OffsetDateTime updatedAt;
 }
